9385a5b8a6a8abf9a52097af4b6aece4e09da143
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2022-06-14 22:02:01 -0500

    stuff

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import pygame as pg
+import pygame.gfxdraw
 from sand import sand
 
 def main():
     pg.init()
     display_size = (800, 600)
-    #resolution = (400, 300)
-    pg.display.set_mode(display_size)
+    resolution = (400, 300)
     pg.display.set_caption("SandSimulation")
-    #window = pg.display.set_mode(resolution)
+    window = pg.display.set_mode(display_size)
+    screen = pg.Surface(resolution)
     running = True
-    pixels = [[0 for a in range(display_size[1])] for b in range(display_size[0])]
+    pixels = [[0 for a in range(resolution[1])] for b in range(resolution[0])]
+    buffer = [[0 for a in range(resolution[1])] for b in range(resolution[0])]
+    clock = pg.time.Clock()
     while running:
-        pg.display.get_surface().fill((0,0,0))
+        clock.tick(30)
+        screen.fill((0,0,0))
         keys = pg.key.get_pressed() 
         if keys[pg.K_w]:
             xM, yM = pg.mouse.get_pos()
-            pixels[xM][yM] = sand(xM, yM)
+            xM = int(xM / (display_size[0]/resolution[0]))
+            yM = int(yM / (display_size[1]/resolution[1]))
+            buffer[xM][yM] = sand(xM, yM)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-        for x in range(display_size[0]):
-            for y in range(display_size[1]):
+        # use the buffer to move the sand and draw it to the screen
+        for x in range(resolution[0]):
+            for y in range(resolution[1]):
+                if buffer[x][y] != 0:
+                    
+                    buffer[x][y].move(buffer[x][y], buffer)
+        for x in range(resolution[0]):
+            for y in range(resolution[1]):
                 if pixels[x][y] != 0:
-                    pixels = pixels[x][y].move(pixels[x][y], pixels)
-        for x in range(display_size[0]):
-            for y in range(display_size[1]):
-                if pixels[x][y] != 0:
-                    pg.display.get_surface().set_at((x, y), pixels[x][y].color)
+                    if pixels[x][y].hasMoved:
+                        pixels[x][y].hasMoved = False
+                    pg.draw.rect(screen, pixels[x][y].color, (x, y, 1, 1))
+        pixels = buffer
+        scaled_win = pg.transform.smoothscale(screen, window.get_size())
+        window.blit(scaled_win, (0, 0))
         pg.display.flip()
     pg.quit()
 
--- sand.py
+++ sand.py
@@ -5,20 +5,22 @@ class sand:
         self.y = y
         self.type = 1
         self.color = (255, 255, 255)
+        self.hasMoved = False
         return self
     def move(point, pixels):
-        if point.y + 1 < len(pixels[0]) and pixels[point.x][point.y + 1] == 0:
-            point.y += 1
+        if point.hasMoved:
+            return
+        if point.y + 1 < len(pixels[0]):
+            if pixels[point.x][point.y + 1] == 0:
+                point.y += 1
+                pixels[point.x][point.y - 1] = 0
+            elif point.x + 1 < len(pixels) and pixels[point.x + 1][point.y + 1] == 0:
+                point.x += 1
+                point.y += 1
+                pixels[point.x - 1][point.y - 1] = 0
+            elif point.x - 1 > -1 and pixels[point.x - 1][point.y + 1] == 0:
+                point.x -= 1
+                point.y += 1
+                pixels[point.x + 1][point.y - 1] = 0
+            point.hasMoved = True
             pixels[point.x][point.y] = point
-            pixels[point.x][point.y - 1] = 0
-        if point.y + 1 < len(pixels[0]) and point.x + 1 < len(pixels) and pixels[point.x + 1][point.y + 1] == 0:
-            point.x += 1
-            point.y += 1
-            pixels[point.x][point.y] = point
-            pixels[point.x - 1][point.y - 1] = 0
-        if point.y + 1 < len(pixels[0]) and point.x - 1 > -1 and pixels[point.x - 1][point.y + 1] == 0:
-            point.x -= 1
-            point.y += 1
-            pixels[point.x][point.y] = point
-            pixels[point.x + 1][point.y - 1] = 0
-        return pixels
